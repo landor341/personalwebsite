0dc01cb32599e72663147b4e3ae79c7bf0ce5d65
Landon Faris <larobis4@gmail.com>
2025-07-02 20:59:11 -0500

    Initial markdown formatting layout design

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import { createServer } from "http";
 import * as dotenv from 'dotenv';
+import fs from 'node:fs/promises';
+import Handlebars from 'handlebars';
+import { markdownToHtml } from "ts-markdown-parser";
 
 dotenv.config(); // Get env variables
 
@@ -16,24 +19,46 @@ const server = createServer(app);
 
 let listenCount = 0;
 
-app.get('/', (req, res) => {
-	res.send("The start of something...");
-	console.log("Served content", listenCount++, "times");
-});
-
-const shutdown = () => {
-	console.log("Shutting down...");
-	server.close(() => {
-		console.log("Server closed, exiting...");
-		process.exit(0);
-	});
+// app.get('/', (req, res) => {
+// 	res.send("The start of something...");
+// 	console.log("Served content", listenCount++, "times");
+// });
+1
+const preprocessMDPages = async () => {
+	// const dir = await fs.opendir("static/markdown");
+	// for await (const entry of dir) {
+	// 	// TODO: Process documents with handlebars
+	// 	Handlebars.precompile()
+	// }
+	const template = (await fs.readFile("src/templates/mdTemplate.hbs")).toString();
+	const mdContent = markdownToHtml(await (await fs.readFile("src/markdown/home.md")).toString());
+	const documentConfig = {
+		mdContent,
+		backgroundImage: "/resources/images/scenery/home.JPG"
+	}
+	const document = Handlebars.compile(template);
+	fs.writeFile("static/home.html", document(documentConfig));
 }
+preprocessMDPages().then(() => {
+	const options = { index: "home.html" };
+	app.use('/', express.static('static', options));
+	app.use('/resources/images', express.static('static/images'))
 
-server.listen(port, () => {
-	console.log(`Server is running on http://localhost:${port}`);
-	process.on('SIGTERM', shutdown);
-	process.on('SIGSTP', shutdown);
-	process.on("SIGINT", shutdown);
+
+	const shutdown = () => {
+		console.log("Shutting down...");
+		server.close(() => {
+			console.log("Server closed, exiting...");
+			process.exit(0);
+		});
+	}
+
+	server.listen(port, () => {
+		console.log(`Server is running on http://localhost:${port}`);
+		process.on('SIGTERM', shutdown);
+		process.on('SIGSTP', shutdown);
+		process.on("SIGINT", shutdown);
+	});
 });
 
 
---
+++ src/markdown/home.md
@@ -0,0 +1,20 @@
+You've discovered my website! It may not be much, but it's the start of a big project!
+
+My name is Landon Faris, a senior computer engineering student at the University of Wisconsin-Milwaukee who has a passion for computer science and DevOps automation. I've been writing code ever since I was 5 years old and this website is meant to act as a testament to the wealth of knowledge and experience that I've acquired throughout my life.
+
+I currently work as a DevOps intern at Rockwell Automation with 3 prior years of experience including 21 months (part-time school year included) as a full stack developer for an Agile team using a MEAN stack, a summer as an intern working under three managers creating various software tools while also gaining experience testing motor drives and PCBs, and a summer working on a software testing team improving a testing automation platform.
+
+This website is fully self-hosted and uses a number of tools to maintain a robust and secure infrastructure.
+- Running locally on a mini PC
+- Machine configured as web server completely declaratively with NixOS
+- DNS Routing through Cloudflare Zero Trust
+- Express Node server running typescript that preprocesses page content using Handlebars and markdown
+
+
+Here's a brief outlook for what else I plan to include here:
+- Gitlab runners (on a private gitlab instance!)
+- Ansible automation for server testing and deployment
+- A blog like platform that can be updated without having to touch the server in any way
+- Nixos configurations for quickly and deterministically deploying new infrastructure
+- Containizered deployment of gitlab runners and redundant web servers
+- Handling of subdomains for my various services
